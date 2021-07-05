bf86cd7f6b69945982cf082db8908ccde438e454
Jay Conner <cleve703@gmail.com>
2021-07-05 16:13:27 +0000

    add temperature conversion functions

--- src/index.js
+++ src/index.js
@@ -1,14 +1,33 @@
 import getCurrentWeather from './modules/api';
 
+let unitType = 'imperial';
+let currentCity = 'Louisville';
+
 function convertKelvinToFarenheit(inputTemp) {
   return Math.round((inputTemp - 273.15) * 1.8 + 32);
 }
 
+function convertKelvinToCelsius(inputTemp) {
+  return Math.round((inputTemp - 273.15));
+}
+
 async function displayCurrentWeather() {
-  const myData = await getCurrentWeather();
-  console.log(`The current temperature is ${convertKelvinToFarenheit(myData.main.temp)}.`);
-  console.log(`The high temperature is ${convertKelvinToFarenheit(myData.main.temp_max)}.`);
-  console.log(`The low temperature is ${convertKelvinToFarenheit(myData.main.temp_min)}.`);
+  const myData = await getCurrentWeather(currentCity);
+  let currentTemp = '';
+  let dailyLow = '';
+  let dailyHigh = '';
+  if (unitType === 'imperial') {
+    currentTemp = convertKelvinToFarenheit(myData.main.temp);
+    dailyLow = convertKelvinToFarenheit(myData.main.temp_min);
+    dailyHigh = convertKelvinToFarenheit(myData.main.temp_max);
+  } else if (unitType === 'metric') {
+    currentTemp = convertKelvinToCelsius(myData.main.temp);
+    dailyLow = convertKelvinToCelsius(myData.main.temp_min);
+    dailyHigh = convertKelvinToCelsius(myData.main.temp_max);
+  }
+  console.log(`The current temperature is ${currentTemp}.`);
+  console.log(`The high temperature is ${dailyLow}.`);
+  console.log(`The low temperature is ${dailyHigh}.`);
 }
 
 displayCurrentWeather();
--- src/modules/api.js
+++ src/modules/api.js
@@ -1,7 +1,7 @@
 const apiKey = '48e569b4a59bddb26d06c68542bc69a6';
 
-export default async function getCurrentWeather() {
-  const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=London&appid=${apiKey}`, { mode: 'cors' });
+export default async function getCurrentWeather(city) {
+  const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`, { mode: 'cors' });
   const weatherData = await response.json();
   return weatherData;
 }
