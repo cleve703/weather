a1a1a3b517ffb06674e5386b9c7e2e86a22f5c27
Jay Conner <cleve703@gmail.com>
2021-07-05 03:44:58 +0000

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,12 @@
+const path = require('path');
+const webpack = require("webpack");
+
+module.exports = {
+  mode: "development",
+  entry: { app: "./src/index.js"},
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'main.js',
+  },
+  mode: 'development'
+};
